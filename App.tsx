9beb4d46c139231a4c9c2303ac81158b995558a7
Curtis Oyakoya <curtsoyaks@gmail.com>
2022-09-19 08:51:37 +0100

    added new packages

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  {NativeBaseProvider , Box} from "native-base"
 
 export default function App() {
   return (
+    <NativeBaseProvider>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
+    </NativeBaseProvider>
   );
 }
 
