80efb2b0b5e82fcd7135e0b5bc74e4e96994faf4
Curtis Oyakoya <curtsoyaks@gmail.com>
2022-09-25 11:20:42 +0100

    added detailed comp

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+import NFTCard from "./Card";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./DataInfo";
+import DetailsBid from "./DetailView";
+import FocusedStatusBar from "./FocusBar";
+import DetailsDesc from './DetailScreen';
+
+export {
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+};
